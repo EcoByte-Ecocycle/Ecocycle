1ba4d793758e95647838f00f8520d3f30b0c4212
shindong96 <2006peter@naver.com>
2023-04-25 01:28:23 +0900

    fix: 테스트 코드 오류 수정

--- Backend/src/test/java/com/ecobyte/ecocycle/acceptance/LoginAcceptanceTest.java
+++ Backend/src/test/java/com/ecobyte/ecocycle/acceptance/LoginAcceptanceTest.java
@@ -1,14 +1,19 @@
 package com.ecobyte.ecocycle.acceptance;
 
 import static org.hamcrest.Matchers.notNullValue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.http.HttpStatus.OK;
 
+import com.ecobyte.ecocycle.dto.response.GoogleProfileResponse;
+import com.ecobyte.ecocycle.support.GoogleClient;
 import io.restassured.RestAssured;
 import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
 
@@ -18,6 +23,9 @@ public class LoginAcceptanceTest {
     @LocalServerPort
     int port;
 
+    @MockBean
+    private GoogleClient googleClient;
+
     @BeforeEach
     public void setUp() {
         RestAssured.port = port;
@@ -26,7 +34,13 @@ public class LoginAcceptanceTest {
     @DisplayName("구글 로그인에 성공하면 200 OK를 응답한다.")
     @Test
     void loginWithGoogle() {
-        // given & when
+        // given
+        given(googleClient.getIdToken(anyString()))
+                .willReturn("something");
+        given(googleClient.getProfileResponse(anyString()))
+                .willReturn(new GoogleProfileResponse("azpi@gmail.com", "azpi"));
+        
+        // when
         final ValidatableResponse response = RestAssured.given().log().all()
                 .accept(MediaType.APPLICATION_JSON_VALUE)
                 .when().post("/login?code=anyCode")
