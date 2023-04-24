5e9fe7a1c75d3cd8007c43b0f302095ff345d53d
shindong96 <2006peter@naver.com>
2023-04-24 21:12:12 +0900

    chore: 프로젝트 생성

---
+++ Backend/src/main/java/com/ecobyte/ecocycle/EcoCycleApplication.java
@@ -0,0 +1,13 @@
+package com.ecobyte.ecocycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoCycleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoCycleApplication.class, args);
+    }
+
+}
