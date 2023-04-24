ddd3e597a4b7d55211a996db1fc6a6ca231125fe
shindong96 <2006peter@naver.com>
2023-04-25 03:44:01 +0900

    test: 로그인 인가 실패 경우 테스트 케이스 추가

--- Backend/src/test/java/com/ecobyte/ecocycle/acceptance/UserAcceptanceTest.java
+++ Backend/src/test/java/com/ecobyte/ecocycle/acceptance/UserAcceptanceTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 import com.ecobyte.ecocycle.dto.response.GoogleProfileResponse;
 import com.ecobyte.ecocycle.support.GoogleClient;
@@ -57,5 +58,22 @@ public class UserAcceptanceTest {
                 .body("nickname", equalTo("azpi"))
                 .body("stamps", equalTo(0))
                 .body("dailyQuiz", equalTo(false));
+    }
+
+    @DisplayName("로그인하지 않은 사용자가 메인페이지 출력을 요청할 겨우 토큰 에러와 401 UnAuthorized 를 응답한다.")
+    @Test
+    void findMyInfo_token_fail() {
+        // given
+        final String accessToken = "fake_access_token";
+
+        // when
+        final ValidatableResponse userResponse = RestAssured.given().log().all()
+                .auth().oauth2(accessToken)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/users/me")
+                .then().log().all();
+
+        // then
+        userResponse.statusCode(UNAUTHORIZED.value());
     }
 }
