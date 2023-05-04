36e31111482f20e99c8c4237302d0bcef960198e
shindong96 <2006peter@naver.com>
2023-05-04 17:53:23 +0900

    test: 관리자 판단하는 테스트를 추가한다.

---
+++ Backend/src/test/java/com/ecobyte/ecocycle/domain/user/UserTest.java
@@ -0,0 +1,30 @@
+package com.ecobyte.ecocycle.domain.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class UserTest {
+
+    @DisplayName("사용자가 관리자인지 확인한다")
+    @Test
+    void isAdmin() {
+        // given & then
+        final User user = new User("azpi", "azpi", "azpi@email.com", Role.ADMIN);
+
+        // then
+        assertThat(user.isAdmin()).isTrue();
+    }
+
+    @DisplayName("사용자가 관리자가 아닌 경우인지 확인한다")
+    @Test
+    void isNotAdmin() {
+        // given & then
+        final User user = new User("azpi", "azpi", "azpi@email.com", Role.USER);
+
+        // then
+        assertThat(user.isAdmin()).isFalse();
+    }
+
+}
