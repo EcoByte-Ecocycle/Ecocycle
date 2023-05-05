e4789696bdba064028b976205ebff58358191f08
shindong96 <2006peter@naver.com>
2023-05-05 17:29:31 +0900

    test: 입력값 누락 시 테스트 케이스 추가

--- Backend/src/test/java/com/ecobyte/ecocycle/acceptance/RecyclingProductAcceptanceTest.java
+++ Backend/src/test/java/com/ecobyte/ecocycle/acceptance/RecyclingProductAcceptanceTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.OK;
 
 import com.ecobyte.ecocycle.dto.request.RecyclingProductRequest;
@@ -36,5 +37,25 @@ public class RecyclingProductAcceptanceTest extends AcceptanceTest {
                 .body("name", equalTo(recyclingProductRequest.getName()))
                 .body("recyclingInfo", equalTo(recyclingProductRequest.getRecyclingInfo()))
                 .body("tip", equalTo(recyclingProductRequest.getTip()));
+    }
+
+    @DisplayName("관리자 제품 추가 기능이 성공하면 200 OK를 응답한다.")
+    @Test
+    void add_error_missingInputValue() {
+        // given
+        given(googleClient.getIdToken(anyString()))
+                .willReturn("something");
+        given(googleClient.getProfileResponse(anyString()))
+                .willReturn(new GoogleProfileResponse(adminEmail, "admin"));
+
+        final String accessToken = getAccessToken();
+        final RecyclingProductRequest recyclingProductRequest = new RecyclingProductRequest("바나나껍질",
+                "종량제에 버려주세요.", null);
+
+        // when
+        final ValidatableResponse errorResponse = post("/products", recyclingProductRequest, accessToken);
+
+        // then
+        errorResponse.statusCode(BAD_REQUEST.value());
     }
 }
