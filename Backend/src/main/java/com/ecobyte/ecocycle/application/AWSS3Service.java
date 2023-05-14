fe6c402b1336ae4e3bb33cf1359c52c271218370
shindong96 <2006peter@naver.com>
2023-05-14 18:19:41 +0900

    feat: S3 presigned uel 요청 기능 구현

---
+++ Backend/src/main/java/com/ecobyte/ecocycle/application/AWSS3Service.java
@@ -0,0 +1,50 @@
+package com.ecobyte.ecocycle.application;
+
+import com.amazonaws.HttpMethod;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.Headers;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import com.ecobyte.ecocycle.dto.response.PresignedUrlResponse;
+import java.net.URL;
+import java.util.Date;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AWSS3Service {
+
+    private final AmazonS3 s3Client;
+
+    @Value("${aws.s3.bucket}")
+    private String bucketName;
+
+    public AWSS3Service(final AmazonS3 s3Client) {
+        this.s3Client = s3Client;
+    }
+
+    public PresignedUrlResponse getImagePresignedUrl() {
+        return new PresignedUrlResponse(getPresignedUrl());
+    }
+
+    private String getPresignedUrl() {
+        String fileName = UUID.randomUUID().toString();
+
+        Date expiration = new Date();
+        long expTimeMillis = expiration.getTime();
+        expTimeMillis += 1000 * 60 * 60; // 1시간
+        expiration.setTime(expTimeMillis);
+
+        GeneratePresignedUrlRequest generatePresignedUrlRequest = new GeneratePresignedUrlRequest(bucketName, fileName)
+                .withMethod(HttpMethod.PUT)
+                .withExpiration(expiration);
+
+        generatePresignedUrlRequest.addRequestParameter(Headers.S3_CANNED_ACL,
+                CannedAccessControlList.PublicRead.toString());
+
+        URL url = s3Client.generatePresignedUrl(generatePresignedUrlRequest);
+
+        return url.toExternalForm();
+    }
+}
