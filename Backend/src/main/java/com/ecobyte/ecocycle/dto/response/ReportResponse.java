ba060cecc45655e203b5992ad2bdf0ae112c65cf
Dongseok Shin <70707629+shindong96@users.noreply.github.com>
2023-05-21 00:29:53 +0900

    [BE] 관리자 제보 확인 기능 구현 (#62)
    * chore: submodule 업데이트
    
    * feat: 제보확인 기능 구현

--- Backend/src/main/java/com/ecobyte/ecocycle/dto/response/ReportResponse.java
+++ Backend/src/main/java/com/ecobyte/ecocycle/dto/response/ReportResponse.java
@@ -1,13 +1,30 @@
 package com.ecobyte.ecocycle.dto.response;
 
+import com.ecobyte.ecocycle.domain.report.Report;
+import com.ecobyte.ecocycle.domain.user.User;
 import lombok.Getter;
 
 @Getter
 public class ReportResponse {
 
     private final Long id;
+    private final String productName;
+    private final String imageUrl;
+    private final Long userId;
+    private final String userNickname;
 
-    public ReportResponse(final Long id) {
+    public ReportResponse(final Long id, final String productName, final String imageUrl, final Long userId,
+                          final String userNickname) {
         this.id = id;
+        this.productName = productName;
+        this.imageUrl = imageUrl;
+        this.userId = userId;
+        this.userNickname = userNickname;
+    }
+
+    public static ReportResponse from(final Report report) {
+        final User user = report.getUser();
+        return new ReportResponse(report.getId(), report.getProductName(), report.getImageUrl(), user.getId(),
+                user.getNickname());
     }
 }
--- Backend/src/main/java/com/ecobyte/ecocycle/presentation/ReportController.java
+++ Backend/src/main/java/com/ecobyte/ecocycle/presentation/ReportController.java
@@ -1,11 +1,14 @@
 package com.ecobyte.ecocycle.presentation;
 
 import com.ecobyte.ecocycle.application.ReportService;
+import com.ecobyte.ecocycle.application.auth.AdminAuthorization;
 import com.ecobyte.ecocycle.dto.request.ReportRequest;
-import com.ecobyte.ecocycle.dto.response.ReportResponse;
+import com.ecobyte.ecocycle.dto.response.ReportsResponse;
+import com.ecobyte.ecocycle.dto.response.SavedReportResponse;
 import com.ecobyte.ecocycle.presentation.auth.AuthorizationPrincipal;
 import com.ecobyte.ecocycle.presentation.auth.LoginAuthorization;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,8 +26,14 @@ public class ReportController {
     }
 
     @PostMapping
-    public ResponseEntity<ReportResponse> add(@AuthorizationPrincipal final Long loginId,
-                                              @RequestBody final ReportRequest request) {
+    public ResponseEntity<SavedReportResponse> add(@AuthorizationPrincipal final Long loginId,
+                                                   @RequestBody final ReportRequest request) {
         return ResponseEntity.ok(reportService.add(loginId, request));
+    }
+
+    @GetMapping
+    @AdminAuthorization
+    public ResponseEntity<ReportsResponse> inquire(@AuthorizationPrincipal final Long loginId) {
+        return ResponseEntity.ok(reportService.findALl());
     }
 }
