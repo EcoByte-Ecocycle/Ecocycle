546cc5146312d359c714afb0555e2a4cd5db1372
judyshin99 <76806400+kiwisae99@users.noreply.github.com>
2023-05-22 18:23:47 +0900

    [FE] input CSS 변경 (#66)
    * 메인 페이지 구현
    
    * 이미지 업로드 구현
    
    * commit
    
    * fix: 동기 처리
    
    * 이미지 업로드 api 작성
    
    * admin addReport 구현
    
    * refactor: 동기 비동기 처리
    
    * server-url-env
    
    * refactor/fe/user-tree-image
    
    * refactor/fe/input-css
    
    ---------
    
    Co-authored-by: shindong96 <2006peter@naver.com>

--- Frontend/front/src/pages/addProduct.jsx
+++ Frontend/front/src/pages/addProduct.jsx
@@ -1,8 +1,8 @@
 import '../styles/App.css';
 import '../styles/reset.css';
-import {useNavigate} from 'react-router-dom';
-import {useState} from 'react';
-import {addProductInfo} from '../api/apis';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { addProductInfo } from '../api/apis';
 
 const AddProduct = () => {
 
@@ -18,10 +18,10 @@ const AddProduct = () => {
         tip: "",
     });
 
-    const {productName, recyclingInfo, tip} = inputs;
+    const { productName, recyclingInfo, tip } = inputs;
 
     const onChange = (e) => {
-        const {name, value} = e.target;
+        const { name, value } = e.target;
 
         setInputs({
             ...inputs,
@@ -47,39 +47,42 @@ const AddProduct = () => {
     return (
         <div>
             <main className="admin_page">
-                <img id="logo_img" src="assets/logo.png" alt="EcoCycle logo" onClick={goAdmin}/> <br/>
-                <img id="admin_img" src="assets/admin.png" alt="Admin logo"/> <br/>
+                <img id="logo_img" src="assets/logo.png" alt="EcoCycle logo" onClick={goAdmin} /> <br />
+                <img id="admin_img" src="assets/admin.png" alt="Admin logo" /> <br />
                 <span id="add_products">
                     <div id="box">
                         <b id="text"> 제품 이름: </b>
-                        <input id="product_name"
-                               name="productName"
-                               placeholder="제품 이름을 입력해주세요"
-                               onChange={onChange}
-                               value={productName}
-                        /> <br/>
+                        <input type="text"
+                            id="product_name"
+                            name="productName"
+                            placeholder="제품 이름을 입력해주세요"
+                            onChange={onChange}
+                            value={productName}
+                        /> <br />
                     </div>
                     <div id="box">
                         <b id="text"> 재활용 정보: </b>
-                        <input id="recycling_info"
-                               name="recyclingInfo"
-                               placeholder="재활용 정보를 입력해주세요"
-                               onChange={onChange}
-                               value={recyclingInfo}
-                        /> <br/>
+                        <input type="text"
+                            id="recycling_info"
+                            name="recyclingInfo"
+                            placeholder="재활용 정보를 입력해주세요"
+                            onChange={onChange}
+                            value={recyclingInfo}
+                        /> <br />
                     </div>
                     <div id="box">
                         <b id="text"> 팁: </b>
-                        <input id="tip"
-                               name="tip"
-                               placeholder="팁을 입력해주세요"
-                               onChange={onChange}
-                               value={tip}
-                        /> <br/>
+                        <input type="text"
+                            id="tip"
+                            name="tip"
+                            placeholder="팁을 입력해주세요"
+                            onChange={onChange}
+                            value={tip}
+                        /> <br />
                     </div>
                 </span>
                 <button className="admin_btn" id="addProduct_btn" onClick={sendInfo}>제품 추가</button>
-                <br/>
+                <br />
             </main>
         </div>
     );
