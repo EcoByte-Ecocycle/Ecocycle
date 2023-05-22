441c77264f45a3e2847a22e4a5cd5418e6528750
shindong96 <2006peter@naver.com>
2023-05-22 18:27:50 +0900

    feat: 퀴즈 추가 기능 마무리 구현

--- Backend/src/main/java/com/ecobyte/ecocycle/application/QuizService.java
+++ Backend/src/main/java/com/ecobyte/ecocycle/application/QuizService.java
@@ -1,5 +1,7 @@
 package com.ecobyte.ecocycle.application;
 
+import com.ecobyte.ecocycle.domain.quiz.Quiz;
+import com.ecobyte.ecocycle.domain.quiz.QuizRepository;
 import com.ecobyte.ecocycle.dto.request.QuizRequest;
 import com.ecobyte.ecocycle.dto.response.QuizResponse;
 import org.springframework.stereotype.Service;
@@ -9,7 +11,16 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class QuizService {
 
+    private final QuizRepository quizRepository;
+
+    public QuizService(final QuizRepository quizRepository) {
+        this.quizRepository = quizRepository;
+    }
+
+    @Transactional
     public QuizResponse add(final QuizRequest quizRequest) {
-        return null;
+        final Quiz quiz = new Quiz(quizRequest.getContent(), quizRequest.getAnswer(), quizRequest.getTip());
+        final Quiz savedQuiz = quizRepository.save(quiz);
+        return QuizResponse.from(savedQuiz);
     }
 }
