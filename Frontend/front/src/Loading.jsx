509598bee17cce073c399110a4e517b650e934ed
judyshin99 <76806400+kiwisae99@users.noreply.github.com>
2023-06-09 05:12:00 +0900

    [FE] 로딩 창 구현 (#89)
    * feature: 로그인 기능 구현

---
+++ Frontend/front/src/Loading.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Loading = () => {
+    return (
+        <div>
+            <div id="loading_page">
+                <p className="text">Loading...</p>
+                <img src="assets/spinner.gif" alt="로딩" width={"10%"} />
+            </div>
+        </div>
+    )
+}
+
+export default Loading;
+
