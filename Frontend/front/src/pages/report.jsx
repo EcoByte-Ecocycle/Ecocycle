f60977a32ebbf0eda5820643ed135ad1b4352f67
신주희 <judyshin99@gmail.com>
2023-06-09 05:14:27 +0900

    feature: 제품 설명과 제보하기 기능 추가

--- Frontend/front/src/pages/report.jsx
+++ Frontend/front/src/pages/report.jsx
@@ -6,9 +6,15 @@ import imageHandler from '../hooks/ImageHandler';
 import { getProductInfo } from '../api/apis';
 import ProductInfoModal from '../modals/productInfo';
 import GoReportModal from '../modals/goReport';
+import Loading from '../Loading';
 
 
 const Report = () => {
+
+    const [loading, setLoading] = useState(false);
+    const startLoading = () => setLoading(true);
+    const endLoading = () => setLoading(false);
+
     const movePage = useNavigate();
 
     function goMain() {
@@ -22,6 +28,20 @@ const Report = () => {
     const [rI, setrI] = useState(false);
     const [tip, setTip] = useState(false);
 
+    const [name2, setName2] = useState(false);
+    const [rI2, setrI2] = useState(false);
+    const [tip2, setTip2] = useState(false);
+
+    const [isSecond, setIsSecond] = useState(false);
+    const [showSecond, setShowSecond] = useState(false);
+
+    const showSecondProduct = () => {
+        setShowSecond(true);
+    }
+
+    const showFirstProduct = () => {
+        setShowSecond(false);
+    }
 
     const openModal = () => setIsModalOpen(true);
     const closeModal = () => setIsModalOpen(false);
@@ -41,62 +61,90 @@ const Report = () => {
             setImgFile(reader.result);
         };
 
+        startLoading();
+
         const presignedUrl = await imageHandler(file);
         localStorage.setItem('presignedUrl', presignedUrl);
         const { info } = await getProductInfo(presignedUrl);
 
-        if (info !== null) {
 
-            if (info.name === "goReport") {
-                openGoReportModal();
-            }
-            else {
-                setName(info.name);
-                setrI(info.recyclingInfo);
-                setTip(info.tip);
+        if (info.products.length === 0) {
+            openGoReportModal();
+            endLoading();
+        }
+        else if (info.products.length > 0) {
+            setName(info.products[0].name);
+            setrI(info.products[0].recyclingInfo);
+            setTip(info.products[0].tip);
 
-                openModal();
+            if (info.products.length === 2) {
+                setName2(info.products[1].name);
+                setrI2(info.products[1].recyclingInfo);
+                setTip2(info.products[1].tip);
+
+                setIsSecond(true);
             }
+            endLoading();
+            openModal();
         }
     };
 
 
     return (
         <div>
+            {loading ? <Loading /> : null}
             <main>
-                <hr className="line" />
-                <section>
+                <div id="wrapper">
                     <img id="logo2_img" src="assets/logo2.png" alt="EcoCycle logo" onClick={goMain} /> <br />
-                    <span id="report_span">
-                        <img id="upload_img" src={imgFile ? imgFile : "assets/report.png"} alt="Upload" />
-                    </span>
-                    <form>
-                        <label className="uploadImage_label" htmlFor="uploadImage_input"> 사진 첨부하기</label>
-                        <input type="file" id="uploadImage_input" accept="image/*" style={{ display: "none" }}
-                            ref={imgRef} onChange={saveImgFile} />
-                    </form>
-                </section>
-                <ProductInfoModal isOpen={isModalOpen} closeModal={closeModal}>
-                    <div>
-                        <div id="productInfoModal_span">
-                            <button className="modalClose_btn" onClick={closeModal}>X</button>
-                            <span className="modal_text">
-                                <div id='productName_div'>
-                                    <span id="title_text"  >이것은 </span>
-                                    <span id="name_text">{name}</span>
-                                    <span id="title_text" >입니다</span>
-                                </div> <br />
-                                <div id="producInfo_text" >{rI}</div> <br />
-                                <div id="tip_text" >Tip. {tip}</div>
-                            </span>
-                        </div>
-                    </div>
-                </ProductInfoModal>
+                    <hr className="top_line" />
+                    <section>
+                        <span id="report_span">
+                            <img id="upload_img" src={imgFile ? imgFile : "assets/report.png"} alt="Upload" />
+                        </span>
+                        <form>
+                            <label className="uploadImage_label" htmlFor="uploadImage_input"> 사진 첨부하기</label>
+                            <input type="file" id="uploadImage_input" accept="image/*" style={{ display: "none" }}
+                                ref={imgRef} onChange={saveImgFile} />
+                        </form>
+                    </section>
+                    <ProductInfoModal isOpen={isModalOpen} closeModal={closeModal}>
+                        <div>
+                            <div id="productInfoModal_span">
+                                <button className="modalClose_btn" onClick={closeModal}>X</button>
+                                <span className="modal_text" style={{ display: showSecond ? "none" : "block" }}>
+                                    <div>
+                                        <div className='productName_div'>
+                                            <span id="title_text"  >이것은 </span>
+                                            <span id="name_text">{name}</span>
+                                            <span id="title_text" >입니다</span>
+                                        </div> <br />
+                                        <div className="productInfo_text" >{rI}</div> <br />
+                                        <div className="tip_text" >Tip. {tip}</div>
+                                    </div>
+                                    <button className="result_btn" style={{ display: isSecond ? "block" : "none" }} onClick={showSecondProduct}>next</button>
+                                </span>
 
-                <GoReportModal isOpen={isGoReportModalOpen}></GoReportModal>
+                                <span className="modal_text" style={{ display: showSecond ? "block" : "none" }}>
+                                    <div>
+                                        <div className='productName_div'>
+                                            <span id="title_text"  >이것은 </span>
+                                            <span id="name_text">{name2}</span>
+                                            <span id="title_text" >입니다</span>
+                                        </div> <br />
+                                        <div className="productInfo_text" >{rI2}</div> <br />
+                                        <div className="tip_text" >Tip. {tip2}</div>
+                                    </div>
+                                    <button className="result_btn" onClick={showFirstProduct}>prev</button>
+                                </span>
+                            </div>
+                        </div>
+                    </ProductInfoModal>
+
+                    <GoReportModal isOpen={isGoReportModalOpen}></GoReportModal>
+                </div>
 
                 <footer id="report_footer">
-                    <hr className="line" />
+                    <hr className="bottom_line" />
                     <img id="copyright2_img" src="assets/copyright2.png" alt="Copyright by EcoByte" />
                 </footer>
             </main>
